4b9efd54ce01ad02695c6d59c67c140b8850094c
jarekl <jarekl@users.sourceforge.net>
2012-05-16 08:41:34 +0200

    Moved UI-related stuff, documentation, run scripts to external segment-ui module.

--- src/main/scripts/segment.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set SCRIPT_DIR=%~d0%~p0
-set PROJECT_HOME=%SCRIPT_DIR%..
-set JARS=%PROJECT_HOME%\lib\*
-set CLASS=net.sourceforge.segment.ui.console.Segment
-
-java -cp "%JARS%" %CLASS% %* 
