e048633f25fa0cf0ec67c95862134e05eca185b3
Jarek Lipski <loomchild@loomchild.net>
2015-09-23 21:24:47 +0200

    Fix segment-ui package
    * wrong main class name
    * no examples dir

--- segment-ui/src/main/scripts/segment.bat
+++ segment-ui/src/main/scripts/segment.bat
@@ -3,6 +3,6 @@
 set SCRIPT_DIR=%~d0%~p0
 set PROJECT_HOME=%SCRIPT_DIR%..
 set JARS=%PROJECT_HOME%\lib\*
-set CLASS=Segment
+set CLASS=net.loomchild.segment.ui.console.Segment
 
 java -cp "%JARS%" %CLASS% %* 
